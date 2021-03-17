8391c085da1e2d05b457b909705a5de5b9b7e027
Gabriel Martins <gabrielvegaz19@hotmail.com>
2021-03-17 17:06:00 -0300

    commit remove wp
    Commit para remoção de algumas pastas.

--- README.md
+++ README.md
@@ -1,2 +1,7 @@
 # Bikcraft-CMS
- Meu projeto Bikcraft como CMS do curso WordPress como CMS da Origamid
+
+Meu projeto Bikcraft como CMS do curso WordPress como CMS da Origamid
+
+Lembrando que a pasta vai dentro do xampp -> htdocs -> bikcraft -> wp-content -> themes
+
+Os plugins utilizados foram o Advanced Custom Field Pro e o W3 Total Cache
--- bikcraft/index.php
+++ bikcraft/index.php
@@ -1,17 +1,29 @@
-<?php
-/**
- * Front to the WordPress application. This file doesn't do anything, but loads
- * wp-blog-header.php which does and tells WordPress to load the theme.
- *
- * @package WordPress
- */
+<?php get_header(); ?>
 
-/**
- * Tells WordPress to load the WordPress theme and output it.
- *
- * @var bool
- */
-define( 'WP_USE_THEMES', true );
+<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
 
-/** Loads the WordPress Environment and Template */
-require __DIR__ . '/wp-blog-header.php';
+<article>
+	<section class="introducao-interna introducao-geral">
+		<div class="container">
+			<h1><?php the_title(); ?></h1>
+		</div>
+	</section>
+
+	<section class="container conteudo-geral">
+		<div class="grid-8">
+			<?php the_content(); ?>
+		</div>
+	</section>
+</article>
+
+<?php endwhile; else: ?>
+
+	<section class="introducao-interna introducao-geral">
+		<div class="container">
+			<h1>Página não encontrada.</h1>
+		</div>
+	</section>
+
+<?php endif; ?>
+
+<?php get_footer(); ?>
